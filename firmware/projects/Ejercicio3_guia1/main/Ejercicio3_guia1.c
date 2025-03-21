f8bc713aa7fcc4afe52795fc51966fc253a83b0d
Micaela Bergamaschi <micaela.bergamaschi@ingenieria.uner.edu.ar>
2025-03-21 15:13:49 -0300

    Ejercicio3 de la guia 1 funcionando

---
+++ firmware/projects/Ejercicio3_guia1/main/Ejercicio3_guia1.c
@@ -0,0 +1,143 @@
+/*! @mainpage Template
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 12/09/2023 | Document creation		                         |
+ *
+ * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include "led.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#define ON 0
+#define OFF 1
+#define TOGGLE 2
+/*==================[macros and definitions]=================================*/
+	struct leds
+{
+    uint8_t mode;       //ON, OFF, TOGGLE
+	uint8_t n_led;        //indica el número de led a controlar
+	uint8_t n_ciclos;  //indica la cantidad de ciclos de ncendido/apagado
+	uint16_t periodo;    //indica el tiempo de cada ciclo
+} my_leds; 
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+void funcion(struct leds* estructura){
+switch (estructura->mode)
+{
+case ON:
+{
+	switch (estructura->n_led)
+		{
+		case 1:
+			LedOn(LED_1);
+			break;
+		case 2:
+			LedOn(LED_2);
+			break;
+		case 3:
+			LedOn(LED_3);
+			break;
+		
+		default:
+			break;
+		}
+		break;
+}
+	
+case OFF:
+	{
+	switch (estructura->n_led)
+		{
+		case 1:
+			LedOff(LED_1);
+			break;
+		case 2:
+			LedOff(LED_2);
+			break;
+		case 3:
+			LedOff(LED_3);
+			break;
+		
+		default:
+			break;
+		}
+		break;
+}
+	break;
+case TOGGLE:
+	{
+	switch (estructura->n_led)
+		{
+		case 1:
+			LedToggle(LED_1);
+			break;
+		case 2:
+			LedToggle(LED_2);
+			break;
+		case 3:
+			LedToggle(LED_3);
+			break;
+		
+		default:
+			break;
+		}
+		break;
+}
+	break;
+
+default:
+	break;
+}
+}
+/*==================[external functions definition]==========================*/
+
+void app_main(void){
+
+LedsInit(); 
+struct leds ledsito; 
+ledsito.mode = ON; 
+ledsito.n_led = 3; 
+ledsito.n_ciclos = 10; 
+ledsito.periodo = 100; 
+funcion(&ledsito);
+
+struct leds ledsito1; 
+ledsito1.mode = ON; 
+ledsito1.n_led = 1; 
+ledsito1.n_ciclos = 10; 
+ledsito1.periodo = 100; 
+funcion(&ledsito1);
+
+struct leds ledsito2; 
+ledsito2.mode = ON; 
+ledsito2.n_led = 1; 
+ledsito2.n_ciclos = 10; 
+ledsito.periodo = 100; 
+funcion(&ledsito1);
+
+
+}
+/*==================[end of file]============================================*/
