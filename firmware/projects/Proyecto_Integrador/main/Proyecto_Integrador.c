06ad49105529eb2f01720e3d5a16aa7e6a963ef8
Micalea Bergamaschi <micaela.bergamaschi@ingenieria.uner.edu.ar>
2025-06-13 16:17:14 -0300

    TP funcionando completo

--- firmware/projects/Proyecto_Integrador/main/Proyecto_Integrador.c
+++ firmware/projects/Proyecto_Integrador/main/Proyecto_Integrador.c
@@ -54,79 +54,70 @@
 #include "switch.h"
 
 /*==================[macros and definitions]=================================*/
-#define PERIODO_MUESTREO 12.5 * 1000 // 80Hz
+#define PERIODO_MUESTREO 12.5 * 10000 // 80Hz
 
 /*==================[internal data definition]===============================*/
 
 TaskHandle_t medir_task_handle = NULL;
-TaskHandle_t temporal_task_handle = NULL; 
+TaskHandle_t temporal_task_handle = NULL;
 float medicion;
-char sMed[10]; 
-char sTiempo[10]; 
-bool conmutarMedicion = true;
-bool medicionContinua = false; 
-bool medicionTemporal = false; 
+char sMed[10];
+char sTiempo[10];
+bool medicionContinua = false;
+bool medicionTemporal = false;
 int auxContador = 0;
 
 /*==================[internal functions declaration]=========================*/
 void FuncTimerTareas(void *param)
 {
 	vTaskNotifyGiveFromISR(medir_task_handle, pdFALSE);
-	vTaskNotifyGiveFromISR(temporal_task_handle, pdFAIL); 
+	vTaskNotifyGiveFromISR(temporal_task_handle, pdFAIL);
 }
 
 void prenderLeds()
 {
-	if (conmutarMedicion)
-		{
 
-			if (medicion < 2)
-			{
-				LedOff(LED_1);
-				LedOff(LED_2);
-				LedOff(LED_3);
-			}
-			else if (medicion < 3)
-			{
-				LedOn(LED_1);
-				LedOff(LED_2);
-				LedOff(LED_3);
-			}
-			else if (medicion < 4)
-			{
-				LedOn(LED_1);
-				LedOn(LED_2);
-				LedOff(LED_3);
-			}
-			else if (medicion >= 4)
-			{
-				LedOn(LED_1);
-				LedOn(LED_2);
-				LedOn(LED_3);
-			}
-			else
-			{
-				LedOff(LED_1);
-				LedOff(LED_2);
-				LedOff(LED_3);
-			}
-		}
-		else
-		{
-			LedOff(LED_1);
-			LedOff(LED_2);
-			LedOff(LED_3);
-		}
+	if (medicion < 2)
+	{
+		LedOff(LED_1);
+		LedOff(LED_2);
+		LedOff(LED_3);
+	}
+	else if (medicion < 3)
+	{
+		LedOn(LED_1);
+		LedOff(LED_2);
+		LedOff(LED_3);
+	}
+	else if (medicion < 4)
+	{
+		LedOn(LED_1);
+		LedOn(LED_2);
+		LedOff(LED_3);
+	}
+	else if (medicion >= 4)
+	{
+		LedOn(LED_1);
+		LedOn(LED_2);
+		LedOn(LED_3);
+	}
+	else
+	{
+		LedOff(LED_1);
+		LedOff(LED_2);
+		LedOff(LED_3);
+	}
 }
 
-void ModoMedicionContinua(){
-	medicionContinua = true;
-	medicionTemporal = false;  
+void ModoMedicionContinua()
+{
+
+	medicionContinua = !medicionContinua;
 }
 
-void ModoMedicionTemporal(){
-	medicionTemporal = true; 
-	medicionContinua = true; 
+void ModoMedicionTemporal()
+{
+	medicionTemporal = true;
 }
 
 static void TareaMedir(void *pvParameter)
@@ -134,56 +125,72 @@ static void TareaMedir(void *pvParameter)
 	while (true)
 	{
 		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-		if(medicionContinua == true){
-			medicion = HX711_get_units(10); // leo el dato que me manda la placa
+		if (medicionContinua == true)
+		{
+			medicion = HX711_get_units(1); // leo el dato que me manda la placa
 			// función para prender los leds
-			prenderLeds(); 
+			prenderLeds();
 			// mandar los datos por puerto serie
 			sprintf(sMed, "%.2f\n", medicion);
 			UartSendString(UART_PC, "$");
 			UartSendString(UART_PC, sMed);
 			UartSendString(UART_PC, ";");
 		}
-	
 	}
 }
 
 static void TareaTemporal(void *pvParameter)
 {
-	float medicionMax = 0; 
+	float umbral = 2;
+	bool inicio_medicion = false;
 	while (true)
 	{
 		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-		if(medicionTemporal)
+		if (medicionTemporal)
 		{
-			
-			medicion = HX711_get_units(10); 
-			prenderLeds(); 
-
-			if(medicion > medicionMax){
-				medicionMax = medicion; 
-			}
-
-			float minimo = medicionMax*0.9; 
-			float maximo = medicionMax*1.1; 
-			//tengo que calcular cuantas veces se mantuvo la medición y multiplicarlo por el periodo de muestreo 
-			if(minimo <= medicion && medicion <= maximo){
-				auxContador++; 
-			}else
+			// tengo que calcular cuantas veces se mantuvo la medición y multiplicarlo por el periodo de muestreo
+			if (!inicio_medicion)
 			{
-				float tiempoSostenido = 12.5 * auxContador;
-				UartSendString(UART_PC, "$");
-				UartSendString(UART_PC, "Se sostuvo durante ");
-				UartSendString(UART_PC, sTiempo); 
-				UartSendString(UART_PC, "s");
-				UartSendString(UART_PC, ";");
-				medicionTemporal = false; 
-				auxContador = 0; 
-			}
+				medicion = HX711_get_units(1);
+				prenderLeds();
 
+				if (medicion > umbral)
+					inicio_medicion = true;
+
+				sprintf(sMed, "%.2f\n", medicion);
+				UartSendString(UART_PC, "$");
+				UartSendString(UART_PC, sMed);
+				UartSendString(UART_PC, ";");
+			}
+			else
+			{
+				if (umbral < medicion)
+				{
+					medicion = HX711_get_units(1);
+					prenderLeds();
+
+					sprintf(sMed, "%.2f\n", medicion);
+					UartSendString(UART_PC, "$");
+					UartSendString(UART_PC, sMed);
+					UartSendString(UART_PC, ";");
+					auxContador++;
+				}
+				else
+				{
+					float tiempoSostenido = 0.125 * auxContador;
+					sprintf(sTiempo, "%.2f\n", tiempoSostenido);
+					UartSendString(UART_PC, "$");
+					UartSendString(UART_PC, "Se sostuvo durante ");
+					UartSendString(UART_PC, sTiempo);
+					UartSendString(UART_PC, "s");
+					UartSendString(UART_PC, ";");
+					medicionTemporal = false;
+					auxContador = 0;
+					inicio_medicion = false;
+				}
+			}
 		}
 	}
-	
 }
 
 /*==================[external functions definition]==========================*/
@@ -192,20 +199,20 @@ void app_main(void)
 	// inits
 	LedsInit();
 	HX711_Init(64, GPIO_18, GPIO_19);
-	HX711_setScale(2182.0); 
-	HX711_tare(10); 
+	HX711_setScale(2182.0);
+	HX711_tare(10);
 
 	SwitchesInit();
-	SwitchActivInt(SWITCH_1, ModoMedicionContinua, NULL); 
+	SwitchActivInt(SWITCH_1, ModoMedicionContinua, NULL);
 	SwitchActivInt(SWITCH_2, ModoMedicionTemporal, NULL);
-	
+
 	timer_config_t timer_medicion = {// configuración del timer para tomar la medición
 									 .timer = TIMER_A,
 									 .period = PERIODO_MUESTREO,
 									 .func_p = FuncTimerTareas,
 									 .param_p = NULL};
 	TimerInit(&timer_medicion);
-	
+
 	serial_config_t my_uart = {// configuración de la uart
 							   .port = UART_PC,
 							   .baud_rate = 9600,
@@ -215,9 +222,9 @@ void app_main(void)
 	// creo que acá tengo que configurar la medición
 
 	// puedo hacer un umbral a partil del cual grafique la fuerza
-	
-	xTaskCreate(&TareaMedir, "toma la medición y la procesa", 500, NULL, 5, &medir_task_handle);
-	xTaskCreate(&TareaTemporal, "mide e informa el tiempo que se sostuvo", 500, NULL, 5, &temporal_task_handle);
+
+	xTaskCreate(&TareaMedir, "toma la medición y la procesa", 4096, NULL, 5, &medir_task_handle);
+	xTaskCreate(&TareaTemporal, "mide e informa el tiempo que se sostuvo", 4096, NULL, 5, &temporal_task_handle);
 
 	TimerStart(timer_medicion.timer);
 }
